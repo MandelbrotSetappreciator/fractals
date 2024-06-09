1bab77d0e5dadbca0420c709ac075a043b5bbdca
MandelbrotSetappreciator <rileyj7902@gmail.com>
2024-06-09 03:01:06 -0400

    Add files via upload

--- mandelbulb.m
+++ mandelbulb.m
@@ -1,56 +1,61 @@
-% Define the size of the volume
-volumeSize = 1000;
+% Dimensions of cube
+Dimension = 1000;
 
-n=8;
+n=8; %order of the mandelbulb
 
 % empty volume array
-volumeData = zeros(volumeSize, volumeSize, volumeSize);
+Data = zeros(Dimension, Dimension, Dimension);
 
-zn=0;
-a=4/(volumeSize*(1-1/volumeSize));
+%below is solution to equations for mapping
+a=4/(Dimension*(1-1/Dimension));
 b=-2-a;
-
-for k=1:volumeSize
-    for l=1:volumeSize
-        for m=1:volumeSize
-                %z=0;
+%looping through cartesian coordinates
+for k=1:Dimension
+    for l=1:Dimension
+        for m=1:Dimension
+                %establish origin
                 q=0;
                 r=0;
                 s=0;
 
-                %c= a*k+b + j*(a*l+b);
+                %map the loop variables to coordinates
                 t=a*k+b;
                 u=a*l+b;
                 v=a*m+b;
+
+                %establish spherical coordinates for origin
                 rho=sqrt(q*q+r*r+s*s);
                 theta=atan2(sqrt(q*q+r*r),s);
                 phi=atan2(r,q);
+
+                %iterate 200 times
                 for p=0:200
-                %zn=z^2+c;
+                %raise coordinate to the nth power
                 qton=rho^n*sin(theta*n)*cos(phi*n);
                 rton=rho^n*sin(theta*n)*sin(phi*n);
                 ston=rho^n*cos(theta*n);
-
+                
+                %add to it the current coordinate (like adding c)
                 qn=qton + t;
                 rn=rton + u;
                 sn=ston + v;
                 
-                %z=zn;
+                %update coordinate (like updating z value);
                 rho=sqrt(qn*qn+rn*rn+sn*sn);
                 theta=atan2(sqrt(qn*qn+rn*rn),sn);
                 phi=atan2(rn,qn);
-                if abs(rho)>=2
+                if abs(rho)>=2 %leave it outside sphere of radius 2
                 break;
                 end
                 end
-                if abs(rho)<2
-                volumeData(k,l,m)=1;
+                if abs(rho)<2 %place a voxel if we never leave
+                Data(k,l,m)=1;
                 end
         end
     end
 end
 
 
-% volshow(volumeData);
-customColormap = 'cyan';
-viewer=volshow(volumeData, 'Colormap', customColormap);
+% volshow(Data);
+colormap = 'cyan'; %cyan is pretty
+volshow(Data, 'Colormap', colormap); %plot the bih
